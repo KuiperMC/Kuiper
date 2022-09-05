e4f95c3dca82c3b294b066a9b52be9602ed75dd2
enimaloc <antoine@enimaloc.fr>
2022-09-05 13:46:38 +0200

    fix(packet): trying to fix the actual bug of the server: terrain not loading...

--- src/main/java/fr/enimaloc/kuiper/network/packet/play/ServerboundClientInformation.java
+++ src/main/java/fr/enimaloc/kuiper/network/packet/play/ServerboundClientInformation.java
@@ -57,26 +57,45 @@ public class ServerboundClientInformation extends SimpleClassDescriptor implemen
         player.chatMode = chatMode;
         player.chatColors = chatColors;
         player.displayedSkinParts = displayedSkinParts;
-        player.rightMainHand = mainHand == 1;
-        player.textFiltering = textFiltering;
-        player.serverListing = serverListing;
-        connection.sendPacket(new ClientboundSetCarriedItem().slot((byte) 4));
+        player.rightMainHand      = mainHand == 1;
+        player.textFiltering      = textFiltering;
+        player.serverListing      = serverListing;
+        connection.sendPacket(new ClientboundSetCarriedItem().slot((byte) 0));
         connection.sendPacket(
-                new ClientboundUpdateRecipe()
-                        .recipes(new ClientboundUpdateRecipe.Recipe
-                                .CraftingShapeless(Identifier.kuiper("test"))
-                                         .group(Identifier.kuiper("group_test"))
-                                         .addIngredient(
-                                                 new ClientboundUpdateRecipe.Recipe.Ingredient(
-                                                         new Slot().id(1)
-                                                                   .count((byte) 1)))
-                                         .result(new Slot().id(2).count((byte) 1))));
+                new ClientboundUpdateRecipe());
+//                        .recipes(new ClientboundUpdateRecipe.Recipe
+//                                .CraftingShapeless(Identifier.kuiper("test"))
+//                                         .group(Identifier.kuiper("group_test"))
+//                                         .addIngredient(
+//                                                 new ClientboundUpdateRecipe.Recipe.Ingredient(
+//                                                         new Slot().id(1)
+//                                                                   .count((byte) 1)))
+//                                         .result(new Slot().id(2).count((byte) 1))));
         connection.sendPacket(new ClientboundUpdateTags()
-                                      .block(new ClientboundUpdateTags.Tag("mineable/axe", 10))
-                                      .entityType(new ClientboundUpdateTags.Tag("beehive_inhabitors", 6))
-                                      .fluid(new ClientboundUpdateTags.Tag("lava", 29))
-                                      .gameEvent(new ClientboundUpdateTags.Tag("allay_can_listen", 0))
-                                      .item(new ClientboundUpdateTags.Tag("wool", 0)));
+                                      .block(new ClientboundUpdateTags.Tag("wool", 35))
+                                      .entityType(new ClientboundUpdateTags.Tag("frog_food", 55, 62))
+                                      .fluid(new ClientboundUpdateTags.Tag("water", 8, 9))
+                                      .fluid(new ClientboundUpdateTags.Tag("lava", 10, 11))
+                                      .gameEvent(new ClientboundUpdateTags.Tag("mob_death",
+                                                                               3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+                                                                               13, 14, 15, 16, 17, 18, 19, 20, 21,
+                                                                               22, 23, 24, 25, 26, 27, 28, 29, 30,
+                                                                               31, 32, 33, 34, 35, 36, 37, 38, 39,
+                                                                               40, 41, 42, 43, 44, 45, 46, 47, 48,
+                                                                               49, 50, 51, 52, 53, 54, 55, 56, 57,
+                                                                               58, 59, 60, 61, 62, 63, 64, 65, 66,
+                                                                               67, 68, 69, 70, 71, 72, 73, 74, 75,
+                                                                               76, 77, 78, 79, 80, 81, 82, 83, 84,
+                                                                               85, 86, 87, 88, 89, 90, 91, 92, 93,
+                                                                               94, 95, 96, 97, 98, 99, 100, 101, 102,
+                                                                               103, 104, 105, 106, 107, 108, 109,
+                                                                               110, 111, 112, 113, 114, 115, 116, 117,
+                                                                               118, 119, 120, 121, 122, 123, 124, 125,
+                                                                               126, 127, 128, 129, 130, 131, 132, 133,
+                                                                               134, 135, 136, 137, 138, 139, 140, 141,
+                                                                               142, 143, 144, 145, 146, 147, 148, 149,
+                                                                               150, 151, 152, 153, 154, 155, 156, 157))
+                                      .item(new ClientboundUpdateTags.Tag("coals", 263, 173)));
         connection.sendPacket(new ClientboundEntityEvent().entityId(0).status(Player.Status.OP_LEVEL_4));
         connection.sendPacket(new ClientboundPlayerInfo().add(connection.player.uuid(),
                                                               connection.player.name(),
